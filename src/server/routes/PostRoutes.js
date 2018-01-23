2d3337d1abc842e7cea42d11eb812dd286739497
saraband <sara@band.com>
2018-01-23 23:53:49 +0100

    commit

--- src/server/routes/PostRoutes.js
+++ src/server/routes/PostRoutes.js
@@ -2,13 +2,13 @@ import express from 'express';
 
 const router = express.Router();
 const posts = [
-	{id: 0, text: 'Hello world'},
-	{id: 1, text: 'Bonjour monde'},
+  {id: 0, text: 'Hello world'},
+  {id: 1, text: 'Bonjour monde'},
 ];
 
 router.get('/postlist', (req, res) => {
-	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify(posts));
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(posts));
 });
 
 export default router;
